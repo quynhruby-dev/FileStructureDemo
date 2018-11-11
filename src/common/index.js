5e1662a6587d72d87132074f7ad73e8816d183d3
quynhruby_dev <Phamngocquynh@users.noreply.github.com>
2018-11-11 10:07:07 +0700

    Add files via upload

---
+++ src/common/index.js
@@ -0,0 +1,11 @@
+import Styles from './Styles'
+import Images from './Images'
+import Colors from './Colors'
+import Configs from './Configs'
+
+export {
+    Styles,
+    Images,
+    Colors,
+    Configs
+}
